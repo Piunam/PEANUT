6986a14b244ad27c5ac0e3da316c2d6358251304
Keshav Dayal <115068840+keshxvdayal@users.noreply.github.com>
2024-08-19 03:17:16 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpRequest
+
+# Create your views here.
+
+def login(request):
+    return render(request,'login.html')
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+def profile(request:HttpRequest):
+    return HttpResponse('Hello!')
