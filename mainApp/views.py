f313a15f02142474634debc83e1c1d1b07f9c223
Keshav Dayal <keshavdayal205@gmail.com>
2024-08-19 03:35:15 +0530

    logout added

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -13,3 +15,8 @@ def home(request):
 
 def profile(request:HttpRequest):
     return HttpResponse('Hello!')
+
+
+def logout_view(request):
+    logout(request)  # This logs out the user
+    return redirect('home')  # Redirect to the home page or login page
--- peanut/urls.py
+++ peanut/urls.py
@@ -23,7 +23,7 @@ from mainApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',views.login, name='login'),
-    path('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('social-auth', include('social_django.urls', namespace='social')),
     path("", views.home, name='home'),
     path('accounts/profile/', views.profile),
